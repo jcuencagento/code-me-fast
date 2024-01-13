16583e413ea180e3e44bae42413741aa7324fb62
Javi Cuenca <jcuencagento@gmail.com>
2024-01-13 03:02:16 +0100

    Init shadcn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './mycomponents/Header';
+import Footer from './mycomponents/Footer';
 
 function App() {
     return (
