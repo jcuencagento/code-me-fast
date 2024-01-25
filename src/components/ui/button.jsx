7b9570c67c2f19a2f03cd120c89a26168a2df514
Javi Cuenca <jcuencagento@gmail.com>
2024-01-25 16:18:55 +0100

    Add different texts and continuous typing

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         game: "hover:bg-yellow-200 text-2xl font-bold text-white/80 hover:text-black/80"
       },
       size: {
-        default: "h-9 px-4 py-2",
+        default: "h-10 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
--- src/mycomponents/Test.jsx
+++ src/mycomponents/Test.jsx
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Test.css'
 
-import { PlayIcon, ResetIcon, ClockIcon, RocketIcon, KeyboardIcon } from "@radix-ui/react-icons";
+import { PlayIcon, ResetIcon, ClockIcon, RocketIcon, KeyboardIcon, StopIcon } from "@radix-ui/react-icons";
  
 import { Alert, AlertDescription, AlertTitle } from "../components/ui/alert"
 import { Badge } from "../components/ui/badge";
 import { Button } from "../components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../components/ui/dropdown-menu"
-  
+
+import texts from '../assets/texts.json'
 
 function Test () {
-    /*const text_code = `function helloWorld(name) {\n
-        console.log('Hello world, I am', name);\n
-    };`;*/
-
-    const text = `Lorem ipsu{ [m dolor sit am;et conse,ctetur adipiscing elit et, purus quam felis suspendisse sed mus taciti fringilla, elementum rutrum tincidunt montes dictumst facilisi enim. Est tincidunt magna ut non diam aliquet ante fermentum fames mus luctus nullam, ornare porttitor nec sem habitasse feugiat duis interdum mollis nisi facilisi. Facilisi tempor a sociosqu purus odio condimentum in nascetur, rutrum suspendisse porttitor cum ante venenatis commodo torquent eu, felis nam enim.`;
-
+    /* Use States */
     const [gameType, setGameType] = useState('Strict mode');
-    const [seconds, setSeconds] = useState(60);
+    const [gameDuration, setGameDuration] = useState(60);
+    const [gameTextType, setGameTextType] = useState('JavaScript');
+    const [gameText, setGameText] = useState(texts[gameTextType][Math.floor(Math.random() * 60)]);
+    const [seconds, setSeconds] = useState(gameDuration);
     const [isActive, setIsActive] = useState(false);
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [correctChars, setCorrectChars] = useState(0);
+    const [totalCorrectChars, setTotalCorrectChars] = useState(0);
     const [incorrectChars, setIncorrectChars] = useState(0);
     const [lastCorrect, setLastCorrect] = useState(true);
     const [wpm, setWPM] = useState(0);
   
-    /* Key press */
+    /* Test logic */
     useEffect(() => {
         const handleKeyPress = (event) => {
             if (event.key === ' ') {
@@ -34,50 +33,63 @@ function Test () {
 
             if (isActive) {
                 if (gameType === 'Strict mode') {
-                    const currentChar = text[currentIndex];
+                    const currentChar = gameText[currentIndex];
                     switch (event.key) {
                         case currentChar:
-                            setCorrectChars((prevCorrectChars) => prevCorrectChars + 1);
+                            setTotalCorrectChars((prevTotalCorrectChars) => prevTotalCorrectChars + 1);
                             setCurrentIndex((currentIndex) => currentIndex + 1);
-                            break;
+                            if (currentIndex === gameText.length - 1) {
+                                setGameText(texts[gameTextType][Math.floor(Math.random() * 30)]);
+                                setCurrentIndex(0);
+                                setIncorrectChars(0);
+                            }
+
+                        break;
 
                         case 'Backspace':
                             if (currentIndex > 0) {
                                 setIncorrectChars((prevIncorrectChars) => (prevIncorrectChars > 0 ? prevIncorrectChars - 1 : 0));
                             }
-                            break;
+
+                        break;
 
                         default:
                             const alphanumeric = /^[a-zA-Z0-9\s.\n{}[\]]$/;
                             if (alphanumeric.test(event.key)) {
                                 setIncorrectChars((prevIncorrectChars) => prevIncorrectChars + 1);
                             }
-                            break;
+
+                        break;
                     }
                 } else {
-                    const currentChar = text[currentIndex];
+                    const currentChar = gameText[currentIndex];
                     switch (event.key) {
                         case currentChar:
-                            setCorrectChars((prevCorrectChars) => prevCorrectChars + 1);
+                            setTotalCorrectChars((prevTotalCorrectChars) => prevTotalCorrectChars + 1);
                             setCurrentIndex((currentIndex) => currentIndex + 1);
                             setLastCorrect(true);
-                            break;
+                            if (currentIndex === gameText.length - 1) {
+                                setIsActive(false);
+                            }
+ 
+                        break;
 
                         default:
-                            const alphanumeric = /^[a-zA-Z0-9\s.\n{}[\]]$/;
+                            const alphanumeric = /^[a-zA-Z0-9\s.\n%{}\-[\]]$/;
                             if (alphanumeric.test(event.key)) {
                                 setCurrentIndex((currentIndex) => currentIndex + 1);
                                 setLastCorrect(false);
                             }
-                            break;
+
+                        break;
                     }
                 }
             }
 
-            if (!isActive && (event.key === text[0] || event.key === 'Enter')) {
+            if (!isActive && (event.key === gameText[0] || event.key === 'Enter')) {
                 setIsActive(true);
-                if (event.key === text[0]) {
-                    setCorrectChars((prevCorrectChars) => prevCorrectChars + 1);
+                if (event.key === gameText[0]) {
+                    setTotalCorrectChars((prevCorrectChars) => prevCorrectChars + 1);
                     setCurrentIndex((currentIndex) => currentIndex + 1);
                 }
             }
@@ -88,29 +100,30 @@ function Test () {
         return () => {
             document.removeEventListener('keydown', handleKeyPress);
         };
-    }, [isActive, currentIndex, text, gameType]);
+    }, [isActive, currentIndex, gameText, gameType, gameTextType]);
 
+    /* Texts formatting */
     const getHighlightedTextStrict = (currentIndex) => {
-        const correctText = text.slice(0, currentIndex);
-        const currentChar = text[currentIndex + incorrectChars];
-        const incorrectText = text.slice(currentIndex + (currentIndex > 0 ? 0 : incorrectChars), currentIndex + incorrectChars);
-        const remainingText = text.slice(currentIndex + 1 + incorrectChars);
+        const correctText = gameText.slice(0, currentIndex);
+        const currentChar = gameText[currentIndex + incorrectChars];
+        const incorrectText = gameText.slice(currentIndex + (currentIndex > 0 ? 0 : incorrectChars), currentIndex + incorrectChars);
+        const remainingText = gameText.slice(currentIndex + 1 + incorrectChars);
 
         return (
             <>
                 <span style={{ color: 'green' }}>{correctText}</span>
                 <span style={{ borderRadius: '0.5vh', backgroundColor: 'red' }}>{incorrectText}</span>
-                <span style={{ borderRadius: '0.5vh', backgroundColor: 'grey' }}>{currentChar}</span>
+                <span style={isActive ? { borderRadius: '0.5vh', backgroundColor: 'grey' } : {}}>{currentChar}</span>
                 {remainingText}
             </>
         );
     };
 
     const getHighlightedTextEasy = (currentIndex) => {
-        const oldText = text.slice(0, currentIndex > 1 ? currentIndex - 1 : 0);
-        const prevChar = text[currentIndex - 1];
-        const currentChar = text[currentIndex];
-        const remainingText = text.slice(currentIndex + 1 + incorrectChars);
+        const oldText = gameText.slice(0, currentIndex > 1 ? currentIndex - 1 : 0);
+        const prevChar = gameText[currentIndex - 1];
+        const currentChar = gameText[currentIndex];
+        const remainingText = gameText.slice(currentIndex + 1 + incorrectChars);
 
         return (
             <>
@@ -118,7 +131,7 @@ function Test () {
                 <span style={lastCorrect ? { color: 'green' } : { color: 'red'}}>
                     {prevChar}
                 </span>
-                <span style={{ borderRadius: '0.5vh', backgroundColor: 'grey' }}>{currentChar}</span>
+                <span style={isActive ? { borderRadius: '0.5vh', backgroundColor: 'grey' } : {}}>{currentChar}</span>
                 {remainingText}
             </>
         );
@@ -127,8 +140,7 @@ function Test () {
     /* Timer */
     useEffect(() => {
         let intervalId;
-
-        if (isActive) {
+        if (isActive && gameDuration !== 'Inf') {
             intervalId = setInterval(() => {
                 setSeconds((prevSeconds) => {
                     if (prevSeconds === 0) {
@@ -144,25 +156,29 @@ function Test () {
         return () => {
             clearInterval(intervalId);
         };
-    }, [isActive]);
+    }, [isActive, gameDuration]);
 
     /* WPM */
     useEffect(() => {
         const calculateWPM = () => {
-            const newWPM = ((correctChars / 5) / ((60.01 - seconds) / 60)).toFixed(1);
-            setWPM(newWPM);
+            if (gameDuration !== 'Inf') {
+                const timing = Number(gameDuration) - seconds + 0.01;
+                const newWPM = ((totalCorrectChars / 5) / (timing / 60)).toFixed(1);
+                setWPM(newWPM < 1000 ? newWPM : 0.0);
+            } else {
+                setWPM(totalCorrectChars);
+            }
         };
 
-        if (seconds % 2 === 0 && seconds !== 60) {
-            calculateWPM();
-        }
-    }, [correctChars, seconds]);
-  
+        calculateWPM();
+    }, [totalCorrectChars, seconds, gameDuration]);
+
+    /* Buttons handlers */
     const startAgainTimer = () => {
-        setSeconds(60);
+        setSeconds(gameDuration);
         setIsActive(true);
         setCurrentIndex(0);
-        setCorrectChars(0);
+        setTotalCorrectChars(0);
         setIncorrectChars(0);
         setWPM(0);
     };
@@ -170,16 +186,22 @@ function Test () {
     const startTimer = () => {
         setIsActive(true);
         setCurrentIndex(0);
-        setCorrectChars(0);
+        setTotalCorrectChars(0);
         setIncorrectChars(0);
         setWPM(0);
+    };
+
+    const stopTimer = () => {
+        setIsActive(false);
+        setSeconds(0);
     };
   
     const resetTimer = () => {
         setIsActive(false);
-        setSeconds(60);
+        setGameText(texts[gameTextType][Math.floor(Math.random() * 30)]);
+        setSeconds(gameDuration);
         setCurrentIndex(0);
-        setCorrectChars(0);
+        setTotalCorrectChars(0);
         setIncorrectChars(0);
         setWPM(0);
     };
@@ -187,24 +209,54 @@ function Test () {
     const handleGameType = (value) => {
         setGameType(value);
         setIsActive(false);
-        setSeconds(60);
+        setSeconds(gameDuration);
         setCurrentIndex(0);
-        setCorrectChars(0);
+        setTotalCorrectChars(0);
         setIncorrectChars(0);
         setWPM(0);
-    }
+    };
+
+    const handleGameDuration = (value) => {
+        setGameDuration(value);
+        setIsActive(false);
+        setSeconds(value);
+        setCurrentIndex(0);
+        setTotalCorrectChars(0);
+        setIncorrectChars(0);
+        setWPM(0);
+    };
+
+    const handleGameTextType = (value) => {
+        setGameTextType(value);
+        setGameText(texts[value][Math.floor(Math.random() * 30)]);
+        setIsActive(false);
+        setSeconds(gameDuration);
+        setCurrentIndex(0);
+        setTotalCorrectChars(0);
+        setIncorrectChars(0);
+        setWPM(0);
+    };
 
     return (
         <div className="container">
             <div className="counters">
-                {(seconds !== 0 && !isActive)
-                    ? <Badge variant="secondary" className="text-xl">
-                        <ClockIcon className="mr-2 h-8 w-8" /> {seconds}
-                    </Badge>
-                    : <Badge variant="destructive" className="text-xl">
-                        <ClockIcon className="mr-2 h-8 w-8" /> {seconds}
-                    </Badge>
-                }
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant={seconds !== 0 && !isActive ? "secondary" : "destructive"} className="text-xl">
+                            <ClockIcon className="mr-2 h-8 w-8" /> {seconds}
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-72">
+                        <DropdownMenuLabel>Choose duration</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuRadioGroup value={gameDuration} onValueChange={(value) => handleGameDuration(value)}>
+                            <DropdownMenuRadioItem value="60">60 seconds</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="30">30 seconds</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="120">2 minutes</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="Inf">No time</DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                         <Button variant="game">{gameType}</Button>
@@ -218,9 +270,31 @@ function Test () {
                         </DropdownMenuRadioGroup>
                     </DropdownMenuContent>
                 </DropdownMenu>
-                <Badge variant={wpm > 0 ? wpm > 40 ? "great" : "improve" : "secondary"} className="text-xl">
-                    <KeyboardIcon className="mr-2 h-8 w-8" /> {wpm} WPM
-                </Badge>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="game">{gameTextType === 'randomTexts' ? 'Random text' : gameTextType}</Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-72">
+                        <DropdownMenuLabel>Game type</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuRadioGroup value={gameTextType} onValueChange={(value) => handleGameTextType(value)}>
+                            <DropdownMenuRadioItem value="JavaScript">JavaScript</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="Python">Python</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="Quotes">Quotes</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="randomTexts">Random text</DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                {gameDuration !== 'Inf' 
+                    ? (
+                        <Badge variant={wpm > 0 ? wpm > 40 ? "great" : "improve" : "secondary"} className="text-xl">
+                            <KeyboardIcon className="mr-2 h-8 w-8" /> {wpm} WPM
+                        </Badge>
+                    ) : (
+                        <Badge variant={wpm > 0 ? "great" : "secondary"} className="text-xl">
+                            <KeyboardIcon className="mr-2 h-8 w-8" /> {wpm} Chars
+                        </Badge>
+                    )}
             </div>
             <div className="sentences">
                 <p>
@@ -228,7 +302,7 @@ function Test () {
                 </p>
             </div>
             <div className="buttons">
-                {seconds === 0 
+                {seconds === 0 || currentIndex === gameText.length
                     ? <Button variant="secondary" onClick={startAgainTimer}>
                         <PlayIcon className="mr-2 h-5 w-5" /> START AGAIN
                     </Button>
@@ -236,20 +310,31 @@ function Test () {
                     ? <Button variant="secondary" onClick={startTimer}>
                         <PlayIcon className="mr-2 h-5 w-5" /> START
                     </Button>
+                    : gameDuration === 'Inf'
+                    ? <Button variant="secondary" onClick={stopTimer}>
+                        <StopIcon className="mr-2 h-5 w-5" /> STOP
+                    </Button>
                     : null
                 }
                 <Button variant="secondary" onClick={resetTimer}>
                     <ResetIcon className="mr-2 h-5 w-5" /> RESET
                 </Button>
             </div>
-            {seconds === 0 
+            {seconds === 0 || currentIndex === gameText.length
                 ? <div className="finish">
                     <Alert variant={wpm > 40 ? "great" : "improve"}>
                         <RocketIcon className="h-8 w-8" />
                         <AlertTitle>Heads up!</AlertTitle>
-                        <AlertDescription>
-                            Your score was {wpm} WPM... {wpm > 40 ? "Incredible work!" : "Keep working!"}
-                        </AlertDescription>
+                        {gameDuration !== 'Inf'
+                            ? (
+                                <AlertDescription>
+                                    Your score was {wpm} WPM... {wpm > 40 ? "Incredible work!" : "Keep working!"}
+                                </AlertDescription>
+                            ) : (
+                                <AlertDescription>
+                                    Your score was {wpm} total correct characters!
+                                </AlertDescription>
+                            )}
                     </Alert>
                 </div>
                 : null
